2252cddcbde9c9a2a0decd5b14ebdd1d33920a2f
zhuoluo123 <1143406981@qq.com>
2018-09-11 17:44:36 +0800

    test

--- MyDemo/FileInfoIO.cs
+++ MyDemo/FileInfoIO.cs
@@ -11,61 +11,67 @@ using System.IO;
 
 namespace MyDemo
 {
-    public partial class FileIO : Form
+    public partial class FileInfoIO : Form
     {
-        public FileIO()
+        public FileInfoIO()
         {
             InitializeComponent();
         }
+
         /// <summary>
-        /// 按钮事件创建文件
+        /// FileInfo类创建文件
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text == string.Empty)///表示空字符串
+
+            if (textBox1.Text == string.Empty)
             {
-                MessageBox.Show("文件名不能为空！");
+                MessageBox.Show("文件名称不能为空！");
             }
             else
             {
-                if (File.Exists(textBox1.Text))
+                FileInfo fileInfo = new FileInfo(textBox1.Text);
+                if (fileInfo.Exists)
                 {
-                    MessageBox.Show("该文件已经存在！");
+                    MessageBox.Show("文件已经存在！");
                 }
-                else {
-                    File.Create(textBox1.Text);//使用File类的Create方法创建文件
+                else
+                {
+                    fileInfo.Create();
                 }
+
             }
+
+
 
         }
         /// <summary>
-        /// 创建文件夹
+        /// 使用DirectoryInfo创建文件夹
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void button2_Click(object sender, EventArgs e)
         {
-            if (textBox2.Text==string.Empty)//判断文件夹名称是否为空
+            if (textBox2.Text == string.Empty)
             {
-                MessageBox.Show("文件夹名称不能为空!");
+
+                MessageBox.Show("文件夹名称不能为空！");
             }
             else
             {
-                if (Directory.Exists(textBox2.Text))///判断此文件夹是否已存在
+                DirectoryInfo directoryInfo = new DirectoryInfo(textBox2.Text);
+                if (directoryInfo.Exists)
                 {
-                    MessageBox.Show("该文件夹已存在");
+                    MessageBox.Show("该文件夹已经存在！");
                 }
                 else
                 {
-                    Directory.CreateDirectory(textBox2.Text);//调用Dirextory类的CreateDirectory方法创建文件夹
+                    directoryInfo.Create();
                 }
 
-
-
             }
-
 
         }
     }

e08150e66e691f4fbba3bf10b51cc657a94e512d
zhuoluo123 <1143406981@qq.com>
2018-09-15 10:42:39 +0800

    test

---
+++ MyDemo/FileDemo2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MyDemo
+{
+    public partial class FileDemo2 : Form
+    {
+        public FileDemo2()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 打开目录
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            textBox1.Text = folderBrowserDialog1.SelectedPath;
+        }
+        /// <summary>
+        /// 提取文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] files = Directory.GetFiles(textBox1.Text);
+            for (int i=0;i<files.Length;i++)
+            {
+                textBox2.Lines=files;
+            }
+
+        }
+        /// <summary>
+        ///关闭窗口
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ MyDemo/GDIGraphics.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyDemo
+{
+    public partial class GDIGraphics : Form
+    {
+        public GDIGraphics()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GDIGraphics_Paint(object sender, PaintEventArgs e)
+        {
+            //创建Graphics对象
+            Graphics graphics = e.Graphics;
+        }
+
+        private void GDIGraphics_Load(object sender, EventArgs e)
+        {
+            Graphics g;
+            g = this.CreateGraphics();
+            //Bitmap bitmap = new Bitmap(@"C:\Is.bmp");
+            ///通过FromImage方法创建Graphics
+            //Graphics g = Graphics.FromImage(bitmap);
+            Brush brush = new SolidBrush(Color.Red);
+            Rectangle rt = new Rectangle(10,10,100,100);
+            g.FillRectangle(brush,rt);
+        }
+
+    }
+}
